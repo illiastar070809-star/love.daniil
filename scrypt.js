37a87ea552e7d906b57d2c9cb4a584a3d6d06d6b
illiastar070809-star <illiastar070809@gmail.com>
2026-02-14 10:13:51 +0200

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,41 @@
+let messageIndex = 0;
+
+const messages = [
+  "Ты уверен? 🥺",
+  "я тебя очень прошу? 💔",
+  "Я ж старался 😭",
+  "Ну пожалуйста 💞",
+  "Я без теья ни как 😔",
+];
+
+function handleNoClick() {
+  const noButton = document.querySelector(".no-button");
+  const yesButton = document.querySelector(".yes-button");
+
+  noButton.textContent = messages[messageIndex];
+  messageIndex = (messageIndex + 1) % messages.length;
+
+  const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+  yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+  window.location.href = "yes_page.html";
+}
+function next(n) {
+  document.getElementById("s" + n).classList.remove("active");
+  document.getElementById("s" + (n + 1)).classList.add("active");
+}
+
+function heart() {
+  const h = document.createElement("div");
+  h.className = "heart";
+  h.innerHTML = "❤️";
+  h.style.left = Math.random() * 100 + "vw";
+  h.style.animationDuration = 4 + Math.random() * 3 + "s";
+  document.body.appendChild(h);
+
+  setTimeout(() => h.remove(), 6000);
+}
+
+setInterval(heart, 300);
